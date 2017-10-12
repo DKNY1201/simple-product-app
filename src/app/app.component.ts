f7a8b335954cf4da6a1bf9660d1075e9bb1f2f79
Quy Tran <quytran288@gmail.com>
2017-10-11 20:10:17 -0500

    Step 1 - Create class Product - Display product data - Practice 2 ways binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+type conditionType = 'new' | 'used' | 'discontinued';
+
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    condition: conditionType;
+    category: string;
+}
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <p>
-      app Works!
-    </p>
-  `,
-  styles: []
+    selector: 'app-root',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{product.name}} details!</h2>
+        <div><label>id: </label>{{product.id}}</div>
+        <div><label>name: </label>{{product.name}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="product.name" type="text">
+        </div>
+    `,
+    styles: []
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Simple product app';
+    product: Product = {
+        id: 1,
+        name: 'iPhone 6s',
+        description: 'Good smart phone',
+        price: 1000,
+        condition: 'new',
+        category: 'Phone'
+}
+;
+
+
 }
